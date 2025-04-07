b1a7da5e7f6b464c04d485b235c600b4ce143861
SJOLK <omarrh0645@gmail.com>
2025-04-07 14:49:55 +0200

    Init

---
+++ src/test/java/fr/diginamic/appliweb/AppliwebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.appliweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
