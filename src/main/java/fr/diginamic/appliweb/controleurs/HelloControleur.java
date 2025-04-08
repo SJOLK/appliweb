367a80102f6163bba3b9b108627eb5fbd7ec3d09
SJOLK <omarrh0645@gmail.com>
2025-04-08 11:33:22 +0200

    TP2

--- src/main/java/fr/diginamic/appliweb/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/appliweb/controleurs/HelloControleur.java
@@ -3,13 +3,24 @@ package fr.diginamic.appliweb.controleurs;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloControleur(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping
-    public String direHello(){
-        return "Hello";
+    public String direHello() {
+        return helloService.salutations();
     }
 }
+
 
