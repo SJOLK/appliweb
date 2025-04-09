066bc482653d58bc7cc0a3bd12c9c2559e454e5a
SJOLK <omarrh0645@gmail.com>
2025-04-09 11:41:22 +0200

    TP6

--- src/main/java/fr/diginamic/appliweb/Ville.java
+++ src/main/java/fr/diginamic/appliweb/Ville.java
@@ -1,20 +1,22 @@
 package fr.diginamic.appliweb;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import org.jetbrains.annotations.NotNull;
 
+
+@Entity
 public class Ville {
-    @NotNull
-    @Min(1)
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @NotNull
-    @Size(min = 2)
     private String nom;
-
-    @Min(1)
     private int nbHabitants;
 
 
@@ -22,20 +24,11 @@ public class Ville {
     }
 
 
-    public Ville(int id, String nom, int nbHabitants) {
-        this.id = id;
+    public Ville(String nom, int nbHabitants) {
         this.nom = nom;
         this.nbHabitants = nbHabitants;
     }
 
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getNom() {
         return nom;
